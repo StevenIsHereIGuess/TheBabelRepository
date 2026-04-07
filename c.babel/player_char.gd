6735963b01ca1a51837c0e4f0c89701cac8b571d
Steven <stevenishereiguess@gmail.com>
2026-04-07 17:56:50 -0400

    Making more progress on the State Machine, still have a good amount to go though

--- c.babel/player_char.gd
+++ c.babel/player_char.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const GRAVITY = 300
@@ -8,46 +7,68 @@ const GRAVITY = 300
 enum PlayerState
 {
 	IDLE,
-	JUMP_SQUAT,
-	MOVE_START,
-	MOVE1, 
-	MOVE2,
-	MOVE3,
-	SKID,
-	TURN,
-	JUMP,
+	JUMP_SQUAT, #Transition between idle & jumping
+	MOVE_START, #Transition between idle & moving
+	SPEED1, #Different lvls of speed
+	SPEED2,
+	SPEED3,
+	SKID, #When velocity & direction are not the same slows down player
+	TURN, #When in skid & velocity = 0, if direction is not 0 turn, if 0 then back to idle
+	JUMP, #Rising upward (Just in case yall dont know how to jump, gotchu Angel)
 	FALL,
-	IDLE_CROUCH,
-	CROUCH,
-	SLIDING,
+	CROUCH, #Idle to crouching transition
+	CROUCHING, #Activley crouching (did I spell that right?)
+	SLIDING, 
+	SLIDE_END, 
+	WALL_CLING, #Its more like a wall slide
+	WALL_CLIMB, #Holding jump on a wall will have you climb for a short while
+	WALL_JUMP, #Pressing jump for 
+	VAULT, #Don't worry abt these for rn, I'll get to it
+	MANTLE, #Same goes for this one
 }
 
 var _state: PlayerState = PlayerState.IDLE
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	state_machine_thingie()
-	jump()
-	move()
-	crouch()
+	constant(delta) #Its literaly just gravity, maybe add interaction w/ slopes later
+	state_switcher(delta) #Decides what the player state SHOULD be
+	state_runner(delta) #Decides what the player can & cant do in said state
 	move_and_slide()
 
-func state_machine_thingie():
-	if is_on_floor():
-		if velocity.x == 0:
-			set_state(PlayerState.IDLE)
-		else:
-			set_state(PlayerState.MOVE1)
-	else:
-		if velocity.y > 0:
-			set_state(PlayerState.FALL)
-		else:
-			set_state(PlayerState.JUMP)
-	if crouch():
-		set_state(PlayerState.CROUCH)
+func constant(delta):
+		# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+func state_switcher(delta):
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction == 0 and velocity.x == 0 and is_on_floor():
+		set_state(PlayerState.IDLE)
+	
+	if direction != 0 and velocity.x == 0 and is_on_floor():
+		set_state(PlayerState.MOVE_START)
+	
+	#if direction != 0 and velocity.x > 0 and velocity.x < 300 and is_on_floor():
+		#set_state(PlayerState.SPEED1)
+	
+	if direction != 0 and velocity.x > 300 and velocity.x < 600 and is_on_floor():
+		set_state(PlayerState.SPEED2)
+	
+	if direction != 0 and velocity.x > 600 and is_on_floor():
+		set_state(PlayerState.SPEED3)
+
+func state_runner(delta):
+	match _state:
+		PlayerState.IDLE:
+			print("IDLE")
+		PlayerState.MOVE_START:
+			print("MOVE_START")
+			state_move_start(delta)
+		PlayerState.SPEED1:
+			print("SPEED1")
+			state_speed1(delta)
 
 func set_state(new_state: PlayerState) -> void:
 	if new_state == _state:
@@ -58,7 +79,7 @@ func set_state(new_state: PlayerState) -> void:
 	match _state:
 		PlayerState.IDLE:
 			print("IDLE")
-		PlayerState.MOVE1:
+		PlayerState.SPEED1:
 			print("MOVE")
 		PlayerState.FALL:
 			print("FALL")
@@ -66,6 +87,22 @@ func set_state(new_state: PlayerState) -> void:
 			print("JUMP")
 		PlayerState.CROUCH:
 			print("CROUCH")
+		
+	
+func state_idle(delta):
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		pass
+		#set_state(PlayerState.SPEED1)
+
+func state_move_start(delta):
+	if PlayerState.MOVE_START and Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+		set_state(PlayerState.SPEED1)
+		velocity.x = 100
+
+func state_speed1(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+
 
 func jump():
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
@@ -74,13 +111,9 @@ func jump():
 		velocity.y = velocity.y * 0.4
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-func move():
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 func crouch():
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if Input.is_action_just_pressed("ui_down") and is_on_floor():
 		$StantdingCollisionShape.disabled = true
 		$CrouchCollisionShape.disabled = false
