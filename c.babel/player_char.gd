e6a3ebe6053aff4c1246c6e2beea57dec1ce6a4d
Steven <stevenishereiguess@gmail.com>
2026-04-10 16:57:41 -0400

    I figured out the state machine, it was actually really simple, but the game is still very buggy, I'll fix it either during the week end or during DAE

--- c.babel/player_char.gd
+++ c.babel/player_char.gd
@@ -4,6 +4,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const GRAVITY = 300
 
+#IMPORTANT FOR LATER REMEMBER ABSOLUTE VALUE abs(), -100 = 100 and 12.2 = 12.2
+
 enum PlayerState
 {
 	IDLE,
@@ -32,43 +34,34 @@ var _state: PlayerState = PlayerState.IDLE
 
 func _physics_process(delta: float) -> void:
 	constant(delta) #Its literaly just gravity, maybe add interaction w/ slopes later
-	state_switcher(delta) #Decides what the player state SHOULD be
-	state_runner(delta) #Decides what the player can & cant do in said state
+	state_runner(delta) #Grabs the repective states funciton when called
 	move_and_slide()
+
+func _ready() -> void:
+	starting_state()
 
 func constant(delta):
 		# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-func state_switcher(delta):
-	var direction := Input.get_axis("ui_left", "ui_right")
+func starting_state(): #just the state the player starts in
+	set_state(PlayerState.IDLE)
 
-	if direction == 0 and velocity.x == 0 and is_on_floor():
-		set_state(PlayerState.IDLE)
-	
-	if direction != 0 and velocity.x == 0 and is_on_floor():
-		set_state(PlayerState.MOVE_START)
-	
-	#if direction != 0 and velocity.x > 0 and velocity.x < 300 and is_on_floor():
-		#set_state(PlayerState.SPEED1)
-	
-	if direction != 0 and velocity.x > 300 and velocity.x < 600 and is_on_floor():
-		set_state(PlayerState.SPEED2)
-	
-	if direction != 0 and velocity.x > 600 and is_on_floor():
-		set_state(PlayerState.SPEED3)
-
-func state_runner(delta):
+func state_runner(delta): #Grabs the function for the current state
 	match _state:
 		PlayerState.IDLE:
 			print("IDLE")
+			idle_state(delta)
 		PlayerState.MOVE_START:
 			print("MOVE_START")
-			state_move_start(delta)
+			move_start_state(delta)
 		PlayerState.SPEED1:
 			print("SPEED1")
-			state_speed1(delta)
+			speed1_state(delta)
+		PlayerState.SKID:
+			print("SKID")
+			skid_state(delta)
 
 func set_state(new_state: PlayerState) -> void:
 	if new_state == _state:
@@ -89,6 +82,53 @@ func set_state(new_state: PlayerState) -> void:
 			print("CROUCH")
 		
 	
+
+# Right side is working as intended but left isn't (its lowkey freaing out :sob:)
+
+func idle_state(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+
+	if velocity.x == 0 and direction == 0:
+		pass
+	if Input.is_action_just_pressed("ui_up"):
+		set_state(PlayerState.JUMP_SQUAT)
+	if direction != 0:
+		set_state(PlayerState.MOVE_START)
+
+func move_start_state(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction != 0:
+		set_state(PlayerState.SPEED1)
+	else:
+		set_state(PlayerState.IDLE)
+	
+
+func speed1_state(delta): 
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var velocity_direction = velocity.x / abs(velocity.x)
+	
+	velocity.x += direction * SPEED
+	
+	if direction != 0 and velocity.x > 0 and velocity_direction != 0:
+		pass
+	if direction == 0 or direction != velocity_direction:
+		set_state(PlayerState.SKID)
+	
+
+func skid_state(delta): #currently debugging ts (its probably really simple and I just don't know it)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var velocity_direction = velocity.x / abs(velocity.x)
+	
+	velocity.x == 0
+	
+	if direction == 0 or direction != velocity_direction and velocity.x > 0:
+		pass
+	if direction == velocity_direction and velocity.x > 0:
+		set_state(PlayerState.SPEED1)
+	if direction == 0 and velocity.x == 0:
+		set_state(PlayerState.IDLE)
+
 func state_idle(delta):
 	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
 		pass
